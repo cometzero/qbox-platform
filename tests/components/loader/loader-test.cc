f19188baec2b50af5986b8dd2af1788b70d0e532
Chanho Park <parkch98@gmail.com>
2026-07-01 20:56:36 +0900

    test(loader): add loader fixtures
    Add loader regression tests together with the binary, ELF, linker, and
    CSV fixtures used by those tests. The generated fixtures stay with the
    loader tests because they are required inputs.
    
    Signed-off-by: Chanho Park <parkch98@gmail.com>

---
+++ tests/components/loader/loader-test.cc
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All Rights Reserved.
+ * Author: GreenSocs 2022
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <systemc>
+
+#include "loader-test-bench.h"
+
+// Simple read into the memory and write with elf file
+TEST_BENCH(LoaderTest, SimpleReadELFFile)
+{
+    uint8_t data;
+    /* Target 1 */
+    ASSERT_EQ(m_initiator.do_read(0x0003, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xde);
+
+    ASSERT_EQ(m_initiator.do_read(0x0002, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xad);
+
+    ASSERT_EQ(m_initiator.do_read(0x0001, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xbe);
+
+    ASSERT_EQ(m_initiator.do_read(0x0000, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xaf);
+}
+
+TEST_BENCH(LoaderTest, SimpleReadData)
+{
+    uint8_t data;
+    /* Target 1 */
+    ASSERT_EQ(m_initiator.do_read(0x0503, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xde);
+
+    ASSERT_EQ(m_initiator.do_read(0x0502, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xad);
+
+    ASSERT_EQ(m_initiator.do_read(0x0501, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xbe);
+
+    ASSERT_EQ(m_initiator.do_read(0x0000, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xaf);
+}
+
+TEST_BENCH(LoaderTest, LoadOnResetOnly)
+{
+    uint8_t data;
+
+    ASSERT_EQ(m_initiator.do_read(0x0503, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_NE(data, 0xde);
+
+    m_loader.reset->write(true);
+    sc_core::wait(sc_core::SC_ZERO_TIME);
+
+    ASSERT_EQ(m_initiator.do_read(0x0503, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xde);
+}
+
+TEST_BENCH(LoaderTest, SimpleReadBinFile)
+{
+    uint8_t data;
+    /* Target 2 */
+    ASSERT_EQ(m_initiator.do_read(0x1003, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xde);
+
+    ASSERT_EQ(m_initiator.do_read(0x1002, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xad);
+
+    ASSERT_EQ(m_initiator.do_read(0x1001, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xbe);
+
+    ASSERT_EQ(m_initiator.do_read(0x1000, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xaf);
+}
+
+// Simple read into the memory and write with csv file
+TEST_BENCH(LoaderTest, SimpleReadCSVFile)
+{
+    uint32_t data;
+    /* Target 3 */
+    ASSERT_EQ(m_initiator.do_read(0x2000, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 11);
+
+    ASSERT_EQ(m_initiator.do_read(0x2004, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 12);
+
+    ASSERT_EQ(m_initiator.do_read(0x2008, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 15);
+
+    uint64_t data64;
+    ASSERT_EQ(m_initiator.do_read(0x200c, data), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data, 0xafafb5b5);
+    ASSERT_EQ(m_initiator.do_read(0x200c, data64), tlm::TLM_OK_RESPONSE);
+    ASSERT_EQ(data64, 0xf00afafb5b5);
+}
+
+int sc_main(int argc, char* argv[])
+{
+    gs::ConfigurableBroker m_broker{};
+    cci::cci_originator orig{ "sc_main" };
+    auto broker_h = m_broker.create_broker_handle(orig);
+    ArgParser ap{ broker_h, argc, argv };
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
